cc4467f5ecca49f9553e2a82e5e774381110a872
Caio <caiorulli@gmail.com>
2017-11-26 20:09:24 -0200

    Validation interactor

--- src/db.js
+++ src/db.js
@@ -1,25 +1,44 @@
 const mysql = require('mysql')
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'root',
-  database: 'subjectpicker'
-})
+const connection = () => {
+  if (process.env.DB === 'umbler') {
+    console.log('Connecting to umbler database...')
+    return mysql.createConnection({
+      host: 'mysql762.umbler.com',
+      user: 'piroquio',
+      password: 'deliciameu',
+      database: 'subjectpicker'
+    })
+  }
+  console.log('Connecting to local database...')
+  return mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'subjectpicker'
+  })
+}
 
 const query = async (query, params = []) =>
   new Promise((resolve, reject) =>
-    connection.query(query, params, function (error, results, fields) {
+    connection().query(query, params, function (error, results, fields) {
       if (error) reject(error)
       resolve(results)
     })
   )
+
+const getStudent = async (id) =>
+  query('SELECT * FROM ESTUDANTES WHERE RA = ?', [id])
 
 const getChoices = async (id) =>
   query('SELECT * FROM ELETIVAS WHERE RA = ?', [id])
 
 const getSubjects = async () =>
   query('SELECT * FROM DISCIPLINAS ORDER BY CH DESC')
+
+const getSubjectsFromChoices = async (choiceIds) => {
+  query('SELECT * FROM DISCIPLINAS WHERE ID in ?', [choiceIds])
+}
 
 const deleteChoices = async (id) =>
   query('DELETE FROM ELETIVAS WHERE RA = ?', [id])
@@ -31,8 +50,10 @@ const insertChoice = async ({ id, choice }) =>
   })
 
 module.exports = {
+  getStudent,
   getChoices,
   getSubjects,
+  getSubjectsFromChoices,
   deleteChoices,
   insertChoice
 }
---
+++ src/interactors.js
@@ -0,0 +1,61 @@
+const database = require('./db')
+
+const makeInteractors = (db = database) => {
+  const buildChoiceTable = async (id) => {
+    const choices = await db.getChoices(id)
+    const subjects = await db.getSubjects()
+
+    return subjects.map((subject) => {
+      let hasChosen
+      if (choices.some((choice) => choice.ID_ELETIVA === subject.ID)) hasChosen = true
+      else hasChosen = false
+
+      const weekDay = [undefined, undefined, undefined, 'Terça', 'Quarta', undefined, 'Sexta', undefined, undefined, 'Terça e sexta']
+      return {...subject, hasChosen, weekDay: weekDay[subject.DIA_SEMANA]}
+    })
+  }
+
+  const updateChoices = async (id, choices) => {
+    if (choices && choices.forEach && validateChoices(id, choices)) {
+      await db.deleteChoices(id)
+      choices.forEach(async (choice) => db.insertChoice(id, choice))
+    } else {
+      throw new Error('Invalid choice payload')
+    }
+  }
+
+  const validateChoices = async (id, choiceIds) => {
+    const student = await db.getStudent(id)
+    const choices = await db.getSubjectsFromChoices(choiceIds)
+    if (!chSumMatches(choices, student)) return false
+    else if (!fridayAvailabilityMatches(choices, student)) return false
+    return true
+  }
+
+  return {
+    buildChoiceTable,
+    updateChoices,
+    validateChoices
+  }
+}
+
+const chSumMatches = (choices, student) => {
+  const sum = (a, b) => a + b
+  const choiceChSum = choices.map(choice => choice.CH).reduce(sum)
+  const availableCh = student.SEMESTRE_PREVISTO === 7
+    ? [student.CH_EM_2018_01, student.CH_EM_2018_01 + 72]
+    : [student.CH_EM_2018_01]
+  return availableCh.includes(choiceChSum)
+}
+
+const fridayAvailabilityMatches = (choices, student) => {
+  const fridayAvailable = student.SEXTA_DISPONIVEL === 1
+  const hasChosenFridaySubject = choices.map(choice => choice.DIA_SEMANA).includes(6)
+  if (hasChosenFridaySubject) {
+    if (fridayAvailable) return true
+    else return false
+  }
+  return true
+}
+
+module.exports = makeInteractors
